617f1df59d8a798dfa57e676a514fe117b25f317
Gaurav <gauravkrdas19@gmail.com>
2024-06-19 21:06:22 +0530

    http request for the workouts

--- gym_bro_app/lib/properties/app_colors.dart
+++ gym_bro_app/lib/properties/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const whiteSmoke = Color(0xFFf4f4f4);
-  static const silver = Color(0xFFc9d2d6);
+  static const silver = Color(0xFFa3aaad);
   static const smokyBlack = Color(0xFF0f1108);
   static const pear = Color(0xFFe1e151);
 }
--- gym_bro_app/lib/screens/homepage.dart
+++ gym_bro_app/lib/screens/homepage.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart' as http;
 
 import '../widget/homescreen_appbar.dart';
 
@@ -11,6 +14,7 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  bool workoutLoading = true;
   late DateTime day;
   List<String> workoutType = [
     'Shoulder',
@@ -24,21 +28,38 @@ class _HomepageState extends State<Homepage> {
   void initState() {
     super.initState();
     day = DateTime.now();
+    getWorkouts();
+  }
+
+  Future<void> getWorkouts() async {
+    String workoutsURL =
+        "https://gym-bro-ef156-default-rtdb.firebaseio.com/workouts.json";
+    final response = await http.get(Uri.parse(workoutsURL));
+    try {
+      if (jsonDecode(response.body) != null) {
+        setState(() {
+          workoutLoading = false;
+        });
+        print(jsonDecode(response.body));
+      }
+    } catch (e) {}
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(top: 40.h, left: 20.w, right: 20.w),
-        child: Column(
-          children: [
-            WorkoutHeading(
-              workoutType: workoutType[day.weekday - 1],
-            ),
-          ],
-        ),
-      ),
+      body: (!workoutLoading)
+          ? Padding(
+              padding: EdgeInsets.only(top: 40.h, left: 20.w, right: 20.w),
+              child: Column(
+                children: [
+                  WorkoutHeading(
+                    workoutType: workoutType[day.weekday - 1],
+                  ),
+                ],
+              ),
+            )
+          : const Center(child: CircularProgressIndicator()),
     );
   }
 }
