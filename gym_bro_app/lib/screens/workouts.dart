0b33d4fa1e50b571a24c453406b0f8985cf16ddc
Gaurav <gauravkrdas19@gmail.com>
2024-06-27 23:44:49 +0530

    workout slider

--- gym_bro_app/lib/screens/workouts.dart
+++ gym_bro_app/lib/screens/workouts.dart
@@ -1,28 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../widget/daywiseWorkouts/workouts_arms.dart';
+import '../widget/daywiseWorkouts/workouts_back.dart';
+import '../widget/daywiseWorkouts/workouts_cardio.dart';
+import '../widget/daywiseWorkouts/workouts_chest.dart';
+import '../widget/daywiseWorkouts/workouts_legs.dart';
+import '../widget/daywiseWorkouts/wourkouts_shoulder.dart';
+
 class WorkoutsPage extends StatelessWidget {
   final Map<String, dynamic> workouts;
-  const WorkoutsPage({
+  final PageController slider = PageController();
+  WorkoutsPage({
     super.key,
     required this.workouts,
   });
 
   @override
   Widget build(BuildContext context) {
-    print(workouts);
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.only(top: 60.h, left: 20.w, right: 20.w),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(
-              Icons.keyboard_arrow_left,
-              size: 40.h,
+            IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.keyboard_arrow_left,
+                size: 40.h,
+              ),
             ),
-            Row(
-              children: [],
+            SizedBox(
+              height: 25.h,
+            ),
+            Expanded(
+              child: PageView(
+                controller: slider,
+                children: [
+                  Shoulders(
+                    workout: workouts['day-1'],
+                  ),
+                  Chest(
+                    workout: workouts['day-2'],
+                  ),
+                  Back(
+                    workout: workouts['day-3'],
+                  ),
+                  Arms(
+                    workout: workouts['day-4'],
+                  ),
+                  Legs(
+                    workout: workouts['day-5'],
+                  ),
+                  Cardio(
+                    workout: workouts['day-6'],
+                  ),
+                ],
+              ),
             )
           ],
         ),
---
+++ gym_bro_app/lib/widget/daywiseWorkouts/workouts_back.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../properties/app_colors.dart';
+
+class Back extends StatelessWidget {
+  final Map<String, dynamic> workout;
+  const Back({
+    super.key,
+    required this.workout,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Workout routine for',
+              style: TextStyle(
+                color: AppColor.silver,
+                fontSize: 20.sp,
+              ),
+            ),
+            Text(
+              workout['targetMuscle'],
+              style: TextStyle(
+                color: AppColor.smokyBlack,
+                fontSize: 45.sp,
+                height: .8.h,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
